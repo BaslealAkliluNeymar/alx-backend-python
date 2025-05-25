82842715c4cbcf8e5251028b60776d3c7b415c6b
BaslealAkliluNeymar <baslealaklilu@gmail.com>
2025-05-25 19:29:07 +0300

    done

---
+++ python-generators-0x00/0-stream_users.py
@@ -0,0 +1,29 @@
+import mysql.connector
+import seed
+from itertools import islice
+def stream_users():
+    """
+    Generator function that yields each user from the provided list of users.
+
+    Args:
+        users (list): A list of user dictionaries.
+
+    Yields:
+        dict: The next user in the list.
+    """
+    
+    db = seed.Seed("localhost", "root", "1209", "ALX_prodev")
+    conn = db.connect_to_prodev()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM user_data")
+
+    for user in cursor:
+        yield user
+    
+
+    cursor.close()
+    db.connect_to_prodev().close()
+
+
+for user in islice(stream_users(), 6):
+    print(user)
---
+++ python-generators-0x00/seed.py
@@ -0,0 +1,105 @@
+import mysql.connector
+import csv
+class Seed:
+    def __init__(self, host, user, password, port=3306):
+        self.host = host 
+        self.user = user 
+        self.port = port
+        self.password = password 
+    
+    def connect_db(self):
+        print(f"Connecting to database at {self.host} with user {self.user}...")
+        db  = mysql.connector.connect(
+            host= self.host, 
+            user = self.user, 
+            password= self.password, 
+            port = self.port
+            )
+        print(db)
+        if not db.is_connected():
+            raise Exception("Failed to connect to the database.")
+        return db
+
+    def create_database(self,connection):
+        try:
+            myConnection = connection.cursor()
+            myConnection.execute("CREATE DATABASE IF NOT EXISTS ALX_prodev")
+        finally:
+            myConnection.close()
+
+    def connect_to_prodev(self):
+        print("Connecting to ALX_prodev database...")
+        db = mysql.connector.connect(
+            host=self.host,
+            user=self.user,
+            password=self.password,
+            database="ALX_prodev"
+        )
+        return db
+    
+    def create_table(self, connection):
+        try:
+            myConnection = connection.cursor()
+            myConnection.execute("""
+                 CREATE TABLE IF NOT EXISTS user_data (
+                    id INT AUTO_INCREMENT PRIMARY KEY,
+                    name VARCHAR(100),
+                    email VARCHAR(100) UNIQUE,
+                    age INT,
+                    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+                )
+            """)
+        finally:
+            myConnection.close()
+
+
+            
+    def insert_data(self, connection, data):
+        myConnection  = connection.cursor()
+
+
+        with open(data, 'r') as csvfile:
+            csv_reader = csv.reader(csvfile)
+            for row in csv_reader:
+                name, email, age = row[0], row[1], int(row[2])
+                myConnection.execute("SELECT * FROM user_data WHERE email = %s", (email,))
+                if myConnection.fetchone() is None:
+                    myConnection.execute(
+                        "INSERT INTO user_data (name, email,age,created_at) VALUES (%s, %s,%s,NOW())",
+                        (name, email,age)
+                    )
+                    connection.commit()
+        myConnection.close()
+        
+
+
+
+
+seed =  Seed(
+    "localhost",   
+    "root",
+    "1209",
+    "3306"
+    )
+
+
+connection = seed.connect_db()
+if connection:
+    seed.create_database(connection)
+    connection.close()
+    print(f"connection successful")
+
+    connection = seed.connect_to_prodev()
+
+    if connection:
+        seed.create_table(connection)
+        seed.insert_data(connection, 'user_data.csv')
+        cursor = connection.cursor()
+        cursor.execute(f"SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = 'ALX_prodev';")
+        result = cursor.fetchone()
+        if result:
+            print(f"Database ALX_prodev is present ")
+        cursor.execute(f"SELECT * FROM user_data LIMIT 5;")
+        rows = cursor.fetchall()
+        print(rows)
+        cursor.close()
